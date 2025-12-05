c146e30b4b1e3527d0b434ad35220cae013421d0
Judith Pivoteau <jpivoteau@ippon.fr>
2025-12-05 21:53:28 +0100

    add player movement and boost flames

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var health = 100
-@export var speed = 100
+@export var speed = 400
 @export var rotation_speed = 4
 
+@onready var boost_flames = $"boost flames"
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+	rotation_degrees = -45
+	
+	
+var rotation_direction = 0
+
+func _physics_process(delta: float) -> void:
+	velocity -= Vector2(velocity).normalized() 
+	
 	if Input.is_action_pressed("ui_left"):
-		rotation = rotation + (rotation_speed * delta)
-	elif Input.is_action_pressed("ui_right"):
 		rotation = rotation + (rotation_speed * delta * - 1)
-		
+	elif Input.is_action_pressed("ui_right"):
+		rotation = rotation + (rotation_speed * delta)
+	if Input.is_action_pressed("ui_accept"):
+		velocity = transform.x * speed
+		boost_flames.visible = true
+	if Input.is_action_just_released("ui_accept"):
+		boost_flames.visible = false;
+	move_and_slide()
 	
 func take_damage(amount: int) -> int:
 	return health - amount
